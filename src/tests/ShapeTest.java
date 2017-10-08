275539db047d48e113b5f035d0b03f3ce173c0c8
Domenick Poster <dposter@mix.wvu.edu>
2017-10-08 16:17:34 -0400

    adding instruction

--- src/tests/ShapeTest.java
+++ src/tests/ShapeTest.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 
 public class ShapeTest {
 
+	/*
+	 * Write a test for Shape's compareTo method.
+	 * 
+	 * Also create SquareTest.java and CircleTest.java JUnit test cases.
+	 * Test each class's constructor method and calculateArea method.
+	 */
 	@Test
 	public void testCompareTo() {
 		fail("Not yet implemented");
